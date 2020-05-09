eb80ed1b04d218330f789816d297c723e72882a4
t_fengyun <t_fengyun@foxmail.com>
2020-05-09 10:45:51 +0800

    第一个版本

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Desc: description info is here
+ * Created by PhpStorm.
+ * User: fengy
+ * Date: 2020/5/9
+ * Time: 10:42
+ */
+require_once  "./vendor/autoload.php";
+use Fengyun\Position\Position;
+Position::Hello();
---
+++ src/Fengyun/Position/Position.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Desc: description info is here
+ * Created by PhpStorm.
+ * User: fengy
+ * Date: 2020/5/9
+ * Time: 10:38
+ */
+namespace Fengyun\Position;
+
+class Position
+{
+    public function __construct()
+    {
+    }
+
+    public static function Hello()
+    {
+        echo "hello world 1";
+    }
+}
